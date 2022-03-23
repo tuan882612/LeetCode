766dfdaceccf2a3bc22b8645054f14ec9b8a118a
tuan trinh <88110221+tuan882612@users.noreply.github.com>
2022-03-22 23:20:03 -0500

    Time: 77 ms (18.83%), Space: 14.4 MB (29.74%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,7 +1,18 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        z = list(zip(nums, [i for i in range(len(nums))]))
-        for i, n in z:
-            if i == target:
-                return n
+        start, end = 0, len(nums) - 1
+        while start <= end:
+            mid = start + (end - start) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] >= nums[start]:
+                if target >= nums[start] and target < nums[mid]:
+                    end = mid - 1
+                else:
+                    start = mid + 1
+            else:
+                if target <= nums[end] and target > nums[mid]:
+                    start = mid + 1
+                else:
+                    end = mid - 1
         return -1
