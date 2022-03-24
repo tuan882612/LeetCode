8df7919c010829356c6ceab8617d1cd461e6c909
tuan trinh <88110221+tuan882612@users.noreply.github.com>
2022-03-24 00:39:25 -0500

    Time: 460 ms (16.18%), Space: 14.1 MB (22.65%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -2,9 +2,16 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         hash = {}
         res = left = 0
-        for start in range(len(s)):
-            if s[start] in hash:
-                left = max(left, hash[s[start]]+1)
-            hash[s[start]] = start
-            res = max(res, start-left+1)
+        for start in range(len(s)+1):
+            temp = s[left:start]
+            for j in temp:
+                if j not in hash:
+                    hash[j] = 1
+                else:
+                    hash[j] += 1
+                if hash[j] > 1:
+                    left += 1
+                    break
+            res = max(res, len(hash))
+            hash.clear()
         return res
