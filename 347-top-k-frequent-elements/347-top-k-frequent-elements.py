54be328462493237e52b6ad6271e9a677c1efa37
tuan trinh <88110221+tuan882612@users.noreply.github.com>
2022-04-08 00:21:02 -0500

    Time: 123 ms (67.50%), Space: 18.6 MB (68.02%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -0,0 +1,10 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        hash = {}
+        for i in nums:
+            if i not in hash:
+                hash[i] = 1
+            else:
+                hash[i] += 1
+        hash = sorted(hash.items(), key = lambda x:x[1], reverse = True)
+        return [i[0] for i in hash[:k]]
