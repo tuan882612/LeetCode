551adbeea13c5cde6c3433c8ba4c7fafa66a03d6
tuan trinh <88110221+tuan882612@users.noreply.github.com>
2022-05-02 11:49:13 -0500

    Time: 172 ms (7.07%), Space: 14.7 MB (63.70%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.py
+++ 905-sort-array-by-parity/905-sort-array-by-parity.py
@@ -1,3 +1,13 @@
+from collections import deque
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        return [i for i in nums if i%2==0]+[i for i in nums if i%2!=0]
+        arr = deque()
+        
+        for i in nums:
+            if i%2 == 0:
+                arr.appendleft(i)
+            else:
+                arr.append(i)
+                
+        return arr
+        
