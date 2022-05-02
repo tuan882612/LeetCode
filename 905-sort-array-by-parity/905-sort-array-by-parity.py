5b2243223c3f8c92aa1cb90d42740d89f11d9873
tuan trinh <88110221+tuan882612@users.noreply.github.com>
2022-05-02 11:47:16 -0500

    Time: 97 ms (59.27%), Space: 14.8 MB (17.91%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.py
+++ 905-sort-array-by-parity/905-sort-array-by-parity.py
@@ -1,13 +1,3 @@
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        arr = []
-        evens = 0
-        for i in nums:
-            if i%2 == 0:
-                arr.insert(evens, i)
-                evens += 1
-            else:
-                arr.append(i)
-                
-        return arr
-        
+        return [i for i in nums if i%2==0]+[i for i in nums if i%2!=0]
