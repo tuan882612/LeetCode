f025174ad70679fef23160315e7e5f73e1c97298
tuan trinh <88110221+tuan882612@users.noreply.github.com>
2022-05-08 07:51:26 -0500

    Time: 46 ms (50.43%), Space: 14 MB (16.18%) - LeetHub

---
+++ 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.py
@@ -0,0 +1,19 @@
+from locale import atoi
+class Solution:
+    def removeDigit(self, number: str, digit: str) -> str:
+        seen = set()
+        res = 0
+
+        for _ in number:
+
+            for j in range(len(number)):
+
+                if number[j] == digit and j not in seen:
+                    seen.add(j)
+                    res = max(atoi("".join(number[:j] + number[j+1:])), res)
+                    break
+
+        return str(res)
+            
+        
+            
