625933b71488a985a552536ceb33b86d8ffd4bf8
tuan trinh <88110221+tuan882612@users.noreply.github.com>
2022-05-22 06:37:08 -0500

    Time: 59 ms (89.27%), Space: 13.8 MB (75.32%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
@@ -3,11 +3,8 @@ class Solution:
         stack = []
         
         for i in words:
-            
-            if not stack:
-                stack.append(i)
                     
-            elif stack and sorted(stack[-1]) != sorted(i):
+            if (stack and sorted(stack[-1]) != sorted(i)) or not stack:
                 stack.append(i)
                 
         return stack
