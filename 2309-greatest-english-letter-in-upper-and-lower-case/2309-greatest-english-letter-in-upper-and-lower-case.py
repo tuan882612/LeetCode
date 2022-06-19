16944f8990e695faec5a133fd22dd758a6d3d017
tuan trinh <88110221+tuan882612@users.noreply.github.com>
2022-06-19 11:01:47 -0500

    Time: 79 ms (20.00%), Space: 14 MB (20.00%) - LeetHub

--- 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.py
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.py
@@ -1,12 +1,12 @@
 class Solution:
     def greatestLetter(self, s: str) -> str:
-        res = ""
-        
+        map = [0]*26 
         for i in s:
-            
-            if i.isupper() and i.lower() in s:
-                
-                if i > res:
-                    res = i
-        
-        return res
+            if i.isupper():# check for uppercase
+                map[ord(i)-ord('A')] |= 1 
+            else:# for lowercase
+                map[ord(i) - ord('a')] |= 2 
+        for i in range(25,-1,-1):
+            if map[i] == 3:
+                return chr(i+ord("A"))# return greatest letter
+        return ''# return empty string if not found
