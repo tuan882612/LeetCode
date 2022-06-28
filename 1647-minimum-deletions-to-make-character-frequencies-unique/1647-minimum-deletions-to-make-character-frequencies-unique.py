116c16b2bc453b28a8b446a626a0157588b1eb89
tuan trinh <88110221+tuan882612@users.noreply.github.com>
2022-06-28 17:06:06 -0500

    Time: 526 ms (9.97%), Space: 14.8 MB (52.19%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minDeletions(self, s: str) -> int:
+        hash = {}
+        res = 0
+
+        for i in s:
+            if i not in hash:
+                hash[i] = 1
+            else:
+                hash[i] += 1
+
+        hash = dict(sorted(hash.items(), key = lambda x:x[1]))
+
+        if len(hash) != len(set(hash.values())):
+            track = []
+
+            for key, val in reversed(hash.items()):
+
+                if val in track:
+
+                    while hash[key] in track and hash[key] != 0:
+                        hash[key] -= 1
+                        res += 1
+
+                track.append(hash[key])
+
+        return res
