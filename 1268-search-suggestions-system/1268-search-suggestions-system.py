2d63d5df75e1c68ceb6a96ae9579c888efdd92db
tuan trinh <88110221+tuan882612@users.noreply.github.com>
2022-07-25 09:33:25 -0500

    Time: 971 ms (15.76%), Space: 25.5 MB (5.23%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,20 +1,33 @@
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        products.sort()
-        pref = ""
-        res = []
-
-        for i in searchWord:
-            pref += i
-            temp = []
-
-            for j in products:
-                if len(temp)<3:
-                    if j.startswith(pref):
-                        temp.append(j)
+        class TrieNode:
+            def __init__(self):
+                self.children = collections.defaultdict(TrieNode)
+                self.h = []
+            
+            def add_sugesstion(self, product):
+                if len(self.h) < 3:
+                    heapq.heappush(self.h, MaxHeapStr(product))
                 else:
-                    break
-
-            res.append(temp)
-
-        return res
+                    heapq.heappushpop(self.h, MaxHeapStr(product))
+            
+            def get_suggestion(self):
+                return sorted(self.h, reverse = True)
+        
+        class MaxHeapStr(str):
+            def __init__(self, string): self.string = string
+            def __lt__(self,other): return self.string > other.string
+            def __eq__(self,other): return self.string == other.string
+        
+        root = TrieNode()
+        for p in products:
+            node = root
+            for char in p:
+                node = node.children[char]
+                node.add_sugesstion(p)
+        
+        result, node = [], root
+        for char in searchWord:
+            node = node.children[char]
+            result.append(node.get_suggestion())
+        return result
