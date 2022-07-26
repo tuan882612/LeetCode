7e123738821a225bbbf024fd6f9369fe51cea137
tuan trinh <88110221+tuan882612@users.noreply.github.com>
2022-07-26 07:30:00 -0500

    Time: 57 ms (53.85%), Space: 13.9 MB (61.54%) - LeetHub

--- 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.py
+++ 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.py
@@ -1,21 +1,10 @@
 class Solution:
     def numberOfPairs(self, nums: List[int]) -> List[int]:
-        hash = {}
-        pair = 0
-        one = 0
-
-        for i in nums:
-            if i not in hash:
-                hash[i] = 1
-            else:
-                hash[i] += 1
-
-        for i in hash.values():
-            if i%2 == 0:
-                pair += i//2
-            else:
-                if i > 1:
-                    pair += i//2
-                one += 1
-
-        return [pair, one]
+        nums_set = set(nums)
+        count = 0
+		
+        for n in nums_set:
+            if nums.count(n) % 2 == 1:
+                count += 1
+                
+        return [(len(nums)-count)//2, count]
