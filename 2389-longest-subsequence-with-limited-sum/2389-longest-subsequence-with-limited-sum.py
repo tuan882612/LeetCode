722ebfd7da5288e8d35654bad821fa1fe7c1a1ad
tuan trinh <88110221+tuan882612@users.noreply.github.com>
2022-08-28 00:29:27 -0500

    Time: 107 ms (100.00%), Space: 14.1 MB (80.00%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
@@ -2,9 +2,8 @@ class Solution:
     def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
         nums.sort()
         psum = [0]
+        
         for n in nums:
             psum.append(psum[-1] + n)
-        ans = []    
-        for q in queries:
-            ans.append(bisect.bisect_right(psum, q) - 1)
-        return ans
+
+        return [bisect.bisect_right(psum, i) - 1 for i in queries]
