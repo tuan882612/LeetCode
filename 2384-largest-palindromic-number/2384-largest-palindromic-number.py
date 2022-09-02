2d691ebcbc40ce1a513e8b55185096cd52935dd6
tuan trinh <88110221+tuan882612@users.noreply.github.com>
2022-09-02 06:08:56 -0500

    Time: 70 ms (95.21%), Space: 15.2 MB (70.99%) - LeetHub

--- 2384-largest-palindromic-number/2384-largest-palindromic-number.py
+++ 2384-largest-palindromic-number/2384-largest-palindromic-number.py
@@ -1,30 +1,7 @@
 from collections import deque
 class Solution:
     def largestPalindromic(self, num: str) -> str:
-        hash = {}
-        for i in num:
-            if i not in hash: hash[i] = 0
-            hash[i] += 1
-        hash = dict(sorted(hash.items()))
-        
-        odd = []
-        res = deque()
-        
-        for k, v in hash.items():            
-            if v%2 != 0:
-                # if v > 1: 
-                hash[k] -= 1
-                odd.append(k)
-                
-        if len(odd) != 0: 
-            res.append(odd[-1])
-        
-        for k, v in hash.items():
-            while hash[k] != 0:
-                res.append(k)
-                res.appendleft(k)
-                hash[k] -= 2
-                
-        res = "".join(res).strip('0')
-        if len(res) == 0: return '0'
-        return res
+        count = Counter(num)
+        res = ''.join(count[i] // 2 * i for i in '9876543210').lstrip('0')
+        mid = max(count[i] % 2 * i for i in count)
+        return (res + mid + res[::-1]) or '0'
