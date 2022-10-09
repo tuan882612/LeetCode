dc63e37215eb601b32b732cd45bfa262610cd577
tuan trinh <88110221+tuan882612@users.noreply.github.com>
2022-10-09 09:26:19 -0500

    Time: 309 ms (25.00%), Space: 14 MB (62.50%) - LeetHub

--- 2432-the-employee-that-worked-on-the-longest-task/2432-the-employee-that-worked-on-the-longest-task.py
+++ 2432-the-employee-that-worked-on-the-longest-task/2432-the-employee-that-worked-on-the-longest-task.py
@@ -1,21 +1,11 @@
 class Solution:
     def hardestWorker(self, n: int, logs: List[List[int]]) -> int:
-#         track = logs[0][1]
-#         res = logs[0][0]
-        
-#         for i in range(1,len(logs)):
-#             t = abs(logs[i-1][1]-logs[i][1])
-#             print(t)
-#             if track < t:
-#                 track = t
-#                 res = logs[i][0]
-
         track = 0
-        cur = 0
+        prev = 0
         res = logs[0][0]
         
         for i, t in logs:
-            num = abs(cur-t)
+            num = abs(prev-t)
 
             if num > track:
                 track = num
@@ -24,5 +14,5 @@ class Solution:
                 track = num
                 res = min(res,i)
                 
-            cur = t
+            prev = t
         return res
