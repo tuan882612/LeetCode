8b01cc0c2a728f381ab562eefb038c530401b63e
tuan trinh <88110221+tuan882612@users.noreply.github.com>
2022-10-22 07:32:06 -0500

    Time: 696 ms (23.93%), Space: 16.7 MB (81.07%) - LeetHub

--- 0561-array-partition/0561-array-partition.py
+++ 0561-array-partition/0561-array-partition.py
@@ -2,16 +2,4 @@ import heapq
 
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
-        heapq.heapify(nums)
-        res = 0
-        temp = []
-        
-        while nums:
-            n = heapq.heappop(nums)
-            temp.append(n)
-            
-            if len(temp) > 1:
-                res += min(temp)
-                temp = []
-            
-        return res
+        return sum(sorted(nums)[::2])
