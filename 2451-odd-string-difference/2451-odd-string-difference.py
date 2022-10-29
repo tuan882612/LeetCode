609ad6319a282e9abee3f0d71cd45078f968eddb
tuan trinh <88110221+tuan882612@users.noreply.github.com>
2022-10-29 15:24:04 -0500

    Time: 67 ms (100.00%), Space: 14.1 MB (33.33%) - LeetHub

--- 2451-odd-string-difference/2451-odd-string-difference.py
+++ 2451-odd-string-difference/2451-odd-string-difference.py
@@ -1,16 +1,15 @@
 class Solution:
     def oddString(self, words: List[str]) -> str:
-        res = []
+        res = {}
         
         for word in words:
-            arr = []
+            arr = tuple(ord(word[i])-ord(word[i-1]) for i in range(1, len(word)))
             
-            for i in range(1, len(word)):
-                arr.append(ord(word[i])-ord(word[i-1]))
+            if arr not in res:
+                res[arr] = [word]
+            else:
+                res[arr].append(word)
                 
-            res.append((word,arr))
-        
-        res = sorted(res, key=lambda x:x[1])
-        
-        if res[0][1] != res[1][1]: return res[0][0]
-        if res[-1][1] != res[0][1]: return res[-1][0]
+        for arr, word in res.items():
+            if len(word) == 1:
+                 return word[0]
