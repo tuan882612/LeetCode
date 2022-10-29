86f266bf3be999b065f769b2a302c5e7ce3dbcea
tuan trinh <88110221+tuan882612@users.noreply.github.com>
2022-10-29 15:18:54 -0500

    Time: 83 ms (33.33%), Space: 13.9 MB (66.67%) - LeetHub

--- 2451-odd-string-difference/2451-odd-string-difference.py
+++ 2451-odd-string-difference/2451-odd-string-difference.py
@@ -3,15 +3,14 @@ class Solution:
         res = []
         
         for word in words:
-            calc = [ord(word[i])-ord(word[i-1]) for i in range(1, len(word))]
-            res.append((word,calc))
+            arr = []
             
+            for i in range(1, len(word)):
+                arr.append(ord(word[i])-ord(word[i-1]))
+                
+            res.append((word,arr))
+        
         res = sorted(res, key=lambda x:x[1])
         
         if res[0][1] != res[1][1]: return res[0][0]
         if res[-1][1] != res[0][1]: return res[-1][0]
-        
-        for i in range(1, len(res)):
-            print(res[i])
-            if res[i][1] != res[i-1][1]:
-                return res[i][0]
