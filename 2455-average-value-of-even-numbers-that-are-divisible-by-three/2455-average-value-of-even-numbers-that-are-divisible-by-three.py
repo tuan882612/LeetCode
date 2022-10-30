d17e77988c61ca49748904610cb5596168a1525e
tuan trinh <88110221+tuan882612@users.noreply.github.com>
2022-10-30 00:04:37 -0500

    Time: 187 ms (42.86%), Space: 14.1 MB (85.71%) - LeetHub

--- 2455-average-value-of-even-numbers-that-are-divisible-by-three/2455-average-value-of-even-numbers-that-are-divisible-by-three.py
+++ 2455-average-value-of-even-numbers-that-are-divisible-by-three/2455-average-value-of-even-numbers-that-are-divisible-by-three.py
@@ -4,9 +4,10 @@ class Solution:
         count = 0
         
         for i in nums:
-            if i%2 == 0 and i%3 == 0:
+            if i%6 == 0:
                 res += i
                 count += 1
+                
         if count == 0:
             return 0
         return res//count
