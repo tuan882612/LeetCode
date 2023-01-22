8fdf2634f80957eb610994c74640262f50eb4325
Tuan Trinh <88110221+tuan882612@users.noreply.github.com>
2023-01-22 08:23:34 -0600

    Time: 1339 ms (41.18%), Space: 20.4 MB (11.76%) - LeetHub

--- 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.py
+++ 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.py
@@ -1,7 +1,5 @@
 class Solution:
     def sortTheStudents(self, score: List[List[int]], k: int) -> List[List[int]]:            
-        arr = sorted([score[i][k] for i in range(len(score))], reverse=True)
-        hash = {score[i][k]:score[i] for i in range(len(score))}
-        return [hash[i] for i in arr]
+        return sorted(score, key=lambda x: -x[k])
         
         
