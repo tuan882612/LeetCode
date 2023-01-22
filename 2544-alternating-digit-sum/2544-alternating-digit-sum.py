93367d338d6d5797bfedc886ab142eec55a8d10a
Tuan Trinh <88110221+tuan882612@users.noreply.github.com>
2023-01-22 07:21:50 -0600

    Time: 26 ms (100.00%), Space: 13.9 MB (50.00%) - LeetHub

--- 2544-alternating-digit-sum/2544-alternating-digit-sum.py
+++ 2544-alternating-digit-sum/2544-alternating-digit-sum.py
@@ -1,14 +1,10 @@
 class Solution:
     def alternateDigitSum(self, n: int) -> int:
-        n = int(str(n)[::-1])
-        div = 1
-        res = 0
-
-        while n > 0:
-            temp = n%10*div
-            div *= -1
-            res += temp
-            n = n//10
-
+        res, sign = 0, 1
+        
+        for i in str(n):
+            res += sign * int(i)
+            sign = -sign
+            
         return res
         
