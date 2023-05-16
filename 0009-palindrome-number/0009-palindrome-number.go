c7ec8cfb5185fe8b488b55a2cafc7719bba91d73
Tuan Trinh <tuan882612@gmail.com>
2023-05-15 23:55:35 -0500

    Time: 15 ms (89.43%), Space: 4.5 MB (85.13%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.go
@@ -0,0 +1,16 @@
+import "strconv"
+
+func isPalindrome(x int) bool {
+    s := strconv.Itoa(x)
+    left, right := 0, len(s)-1
+    
+    for left < right {
+        if s[left] != s[right] {
+            return false
+        }
+        left++
+        right--
+    }
+    
+    return true
+}
