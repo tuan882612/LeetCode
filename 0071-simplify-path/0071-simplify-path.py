25f8bbe9061d76dba5a0cedab19a1f129997d312
Tuan Trinh <tuan882612@gmail.com>
2023-08-26 16:44:59 -0500

    Time: 41 ms (71.86%), Space: 16.5 MB (25.79%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -1,14 +1,10 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        ref = '../ '
-        if len(path) == 1 and path not in ref:
-            return '/'
-
         dirs = path.split('/')
         res = []
         
         for dir in dirs:
-            if dir not in ref:
+            if dir not in '../ ':
                 res.append(dir)
             elif dir == '..' and res:
                 res.pop()
