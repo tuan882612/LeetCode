8866b8f7d4863bc2883ee20802c04996ca5288d2
Tuan Trinh <tuan882612@gmail.com>
2023-08-28 00:46:38 -0500

    Time: 40 ms (75.50%), Space: 16.1 MB (99.31%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,11 +1,4 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n <= 1:
-            return n
-        
-        h = [0, 1]
-        
-        for i in range(2, n):
-            h[i%2] = h[0] + h[1]
-
-        return h[0] + h[1]
+        golden_ratio = (1 + 5 ** 0.5) / 2
+        return int((golden_ratio ** n + 1) / 5 ** 0.5)
