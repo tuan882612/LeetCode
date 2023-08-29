632e96c52e244078c1323050d48ce68a975ac12c
Tuan Trinh <tuan882612@gmail.com>
2023-08-29 00:02:22 -0500

    Time: 47 ms (39.91%), Space: 16.4 MB (38.52%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -0,0 +1,19 @@
+class Solution:
+    def generate(self, n: int) -> List[List[int]]:
+        if n == 1:
+            return [[1]]
+        elif n == 2:
+            return [[1], [1,1]]
+
+        res = [[1], [1,1]]
+
+        for i in range(2, n):
+            temp = [1]
+
+            for j in range(1,len(res[i-1])):
+                temp.append(res[i-1][j-1]+res[i-1][j])
+
+            temp.append(1)
+            res.append(temp)
+
+        return res
