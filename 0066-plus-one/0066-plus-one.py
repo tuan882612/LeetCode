956715e97a33e02a633c08ee8abf222d7ce03d4e
Tuan Trinh <tuan882612@gmail.com>
2023-09-13 03:25:36 -0500

    Time: 42 ms (54.58%), Space: 16.2 MB (90.65%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,18 +1,8 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        digits[-1] += 1
-        if digits[-1] < 10:
-            return digits
-        
-        dq = deque(digits)
+        num = digits[0]
 
-        for i in range(len(dq)-2, -1, -1):
-            if dq[i+1] > 9:
-                dq[i] += 1
-                dq[i+1] = 0
+        for d in digits[1:]:
+            num = num*10 + d
 
-        if dq[0] > 9:
-            dq[0] = 0
-            dq.appendleft(1)
-
-        return dq
+        return [int(i) for i in list(str(num+1)) ]
