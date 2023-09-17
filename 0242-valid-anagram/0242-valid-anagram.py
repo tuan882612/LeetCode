c3d69f65f119a5d7c02d6331adc3f3ad377b2ec7
Tuan Trinh <tuan882612@gmail.com>
2023-09-16 23:02:30 -0500

    Time: 43 ms (94.96%), Space: 16.8 MB (85.12%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,18 +1,3 @@
-from collections import Counter
-
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        ref = Counter(s)
-        
-        for i in t:
-            if i not in ref:
-                return False
-            else:
-                ref[i] -= 1
-                if ref[i] < 0:
-                    return False
-                
-        if sum(ref.values()) > 0:
-            return False
-            
-        return True
+        return Counter(s) == Counter(t)
