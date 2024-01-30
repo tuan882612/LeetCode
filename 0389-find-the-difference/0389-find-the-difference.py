196ba0f6442cfd8ddcbb172de770fcd584e88d3b
Tuan Trinh <tuan882612@gmail.com>
2024-01-30 14:41:01 -0600

    Time: 32 ms (92.59%), Space: 16.5 MB (68.32%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,9 +1,3 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        n1 = n2 = 0
-
-        for i in range(len(s)):
-            n1 += ord(s[i])
-            n2 += ord(t[i])
-
-        return chr((n2 + ord(t[len(t)-1])) - n1)
+        return chr(sum(ord(c) for c in t) - sum(ord(c) for c in s))
