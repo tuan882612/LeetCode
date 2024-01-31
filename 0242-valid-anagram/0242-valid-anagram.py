c5f1357cf98282a441a2661682d60597bac2c758
Tuan Trinh <tuan882612@gmail.com>
2024-01-31 02:08:30 -0600

    Time: 40 ms (94.2%), Space: 16.9 MB (80.08%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,6 +1,4 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        for i in 'qwertyuiopasdfghjklzxcvbnm':
-            if s.count(i)!=t.count(i):
-                return False
-        return True
+        hs, ht = Counter(s), Counter(t)
+        return hs == ht
