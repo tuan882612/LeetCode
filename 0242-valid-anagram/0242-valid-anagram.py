60b24bbbdb0d4e11bccbb12d97076da9537cfa94
Tuan Trinh <tuan882612@gmail.com>
2024-04-04 06:01:28 -0500

    Time: 41 ms (89.89%), Space: 17 MB (34.25%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,18 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        count = [0] * 26
-        
-        # Count the frequency of characters in string s
-        for x in s:
-            count[ord(x) - ord('a')] += 1
-        
-        # Decrement the frequency of characters in string t
-        for x in t:
-            count[ord(x) - ord('a')] -= 1
-        
-        # Check if any character has non-zero frequency
-        for val in count:
-            if val != 0:
-                return False
-        
-        return True
+        return Counter(s) == Counter(t)
