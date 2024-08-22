b35448431b85356b060ccb7687cf47908c04dc7c
Tuan Trinh <tuan882612@gmail.com>
2024-08-21 19:26:23 -0500

    Time: 58 ms (69.32%), Space: 17.8 MB (28.92%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        ht = {}
-        
+        table = {}
+
         for i, v in enumerate(nums):
-            n = target-v
-            if n not in ht:
-                ht[v] = i
+            num = target - v
+            if num not in table:
+                table[v] = i
             else:
-                return [ht[n], i]
-            
+                return [table[num], i]
+
         return []
